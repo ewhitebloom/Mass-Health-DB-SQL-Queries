dbf2bb59d12e548f8320d509aca4edbf33ef127d
ewhitebloom <ewhitebloom@gmail.com>
2014-03-17 17:07:11 -0400

    More

--- db/questions.rb
+++ db/questions.rb
@@ -21,10 +21,10 @@ TownHealthRecord.where(:town => (TownHealthRecord.where('per_capita_income_2000 
 TownHealthRecord.where("town ilike 'b%'").order('total_population_2005 desc').limit(1)
 
 # -- Of the 10 towns with the highest percent publicly financed prenatal care, are any of them also the top 10 for total infant deaths?
-select town, percent_publicly_financed_prenatal_care_2005_to_2008, infant_mortality_rate_per_thousand_2005_to_2008 from town_health_records where infant_mortality_rate_per_thousand_2005_to_2008 is not null and percent_publicly_financed_prenatal_care_2005_to_2008 is not null order by infant_mortality_rate_per_thousand_2005_to_2008, percent_publicly_financed_prenatal_care_2005_to_2008 desc limit 6;
+TownHealthRecord.where('infant_mortality_rate_per_thousand_2005_to_2008 is not null').where('percent_publicly_financed_prenatal_care_2005_to_2008 is not null').order('infant_mortality_rate_per_thousand_2005_to_2008').order('percent_publicly_financed_prenatal_care_2005_to_2008 desc ').limit(6)
 
 # -- Which town has the highest percent multiple births?
-select town, percent_multiple_births_2005_to_2008 from town_health_records where percent_multiple_births_2005_to_2008 is not null order by  percent_multiple_births_2005_to_2008 desc limit 1;
+TownHealthRecord.where('percent_multiple_births_2005_to_2008 is not null').order('percent_multiple_births_2005_to_2008 desc').limit(1)
 
 # -- What is the percent adequacy of prenatal care in that town?
 select town, percent_adequacy_pre_natal_care from town_health_records where percent_multiple_births_2005_to_2008 is not null order by  percent_multiple_births_2005_to_2008 desc limit 1;
