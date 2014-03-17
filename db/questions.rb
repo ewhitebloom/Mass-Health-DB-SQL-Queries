ab6ff10040e08137a569660468f208a983034706
ewhitebloom <ewhitebloom@gmail.com>
2014-03-17 16:58:51 -0400

    More

--- db/questions.rb
+++ db/questions.rb
@@ -15,10 +15,10 @@ TownHealthRecord.where('infant_mortality_rate_per_thousand_2005_to_2008 IS NOT N
 
 # -- Of the 5 towns with the highest per capita income, which one has the highest number of people below the poverty line?
 select town from town_health_records where town in (select town from town_health_records where per_capita_income_2000 IS NOT NULL AND  percent_persons_below_poverty_2000 IS NOT NULL order by per_capita_income_2000 desc, percent_persons_below_poverty_2000 desc limit 5) order by percent_persons_below_poverty_2000 desc limit 1;
-TownHealthRecord.where(:town => (TownHealthRecord.where('per_capita_income_2000 IS NOT NULL').where('percent_persons_below_poverty_2000 IS NOT NULL').order('per_capita_income_2000 desc').order('percent_persons_below_poverty_2000 desc').limit(5))).order('percent_persons_below_poverty_2000 desc').limit(1)
+TownHealthRecord.where(:town => (TownHealthRecord.where('per_capita_income_2000 IS NOT NULL').where('percent_persons_below_poverty_2000 IS NOT NULL').order('per_capita_income_2000 desc').limit(5))).order('percent_persons_below_poverty_2000 desc').limit(1)
 
 # -- Of the towns that start with the letter b, which has the highest population?
-select town, total_population_2005 from town_health_records where town ilike 'b%' order by total_population_2005 desc limit 1;
+TownHealthRecord.where("town ilike 'b%'").order('total_population_2005 desc').limit(1)
 
 # -- Of the 10 towns with the highest percent publicly financed prenatal care, are any of them also the top 10 for total infant deaths?
 select town, percent_publicly_financed_prenatal_care_2005_to_2008, infant_mortality_rate_per_thousand_2005_to_2008 from town_health_records where infant_mortality_rate_per_thousand_2005_to_2008 is not null and percent_publicly_financed_prenatal_care_2005_to_2008 is not null order by infant_mortality_rate_per_thousand_2005_to_2008, percent_publicly_financed_prenatal_care_2005_to_2008 desc limit 6;
